a13d7fd8e4c470a5e6feacbd39beadf592cedecc
aravindaqa <ariga.aravind@gmail.com>
2026-03-06 07:39:29 +0530

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+// Test: EPAM - Navigate to Client Work via Services menu
+// Initial scaffold. The implementation will be added in a follow-up edit.
+
+test('EPAM: navigate to Client Work via Services menu', async ({ page }) => {
+  // TODO: implement test steps
+});
