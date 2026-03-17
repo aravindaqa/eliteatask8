604da5acbfb73370b5c1391f33d7c36c33bf508b
aravindaqa <ariga.aravind@gmail.com>
2026-03-17 10:39:27 +0530

    Delete tests/playwright/epam-client-work.spec.ts

--- tests/playwright/epam-client-work.spec.ts
+++
@@ -1,8 +0,0 @@
-/* Playwright test: EPAM Client Work navigation */
-import { test, expect } from '@playwright/test';
-
-test.describe('EPAM - Client Work navigation', () => {
-  test('should navigate from Services -> Explore Our Client Work and show Client Work text', async ({ page }) => {
-    // TODO: implement test steps
-  });
-});
